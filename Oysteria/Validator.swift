ff353f758eb2dc88f276c54a9c4c59864a05294e
Stevhen <stevhen.stevhen@bhinneka.com>
2022-07-20 16:56:58 +0700

    initial commit

---
+++ Oysteria/Validator.swift
@@ -0,0 +1,14 @@
+//
+//  Validator.swift
+//  Oysteria
+//
+//  Created by Stevhen on 20/07/22.
+//
+
+import Foundation
+
+public struct Validator {
+  public static func sayHello() {
+    print("Hello World from Oysteria")
+  }
+}
